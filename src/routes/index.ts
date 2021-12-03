46ee14609c6df13fe5b351a00bbaf64ccf982364
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-12-03 17:07:59 +0800

    feat(other): add router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,8 +4,33 @@ import loadable from "@loadable/component";
 
 export const routes: RouteProps[] = [
   {
+    path: "/album-detail",
+    component: loadable(() => import("../pages/AlbumDetail")),
+    exact: true,
+  },
+  {
     path: "/home",
     component: loadable(() => import("../pages/Home")),
+    exact: true,
+  },
+  {
+    path: "/login-page",
+    component: loadable(() => import("../pages/LoginPage")),
+    exact: true,
+  },
+  {
+    path: "/music-detail",
+    component: loadable(() => import("../pages/MusicDetail")),
+    exact: true,
+  },
+  {
+    path: "/search-page",
+    component: loadable(() => import("../pages/SearchPage")),
+    exact: true,
+  },
+  {
+    path: "/user-detail",
+    component: loadable(() => import("../pages/UserDetail")),
     exact: true,
   },
 ];
