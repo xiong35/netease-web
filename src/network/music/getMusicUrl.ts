2c1a83776ab1a8eecc19fcf13d69ba58bc07ca9b
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-12-07 17:31:57 +0800

    feat(network): add getMusicUrlReq

---
+++ src/network/music/getMusicUrl.ts
@@ -0,0 +1,28 @@
+import _request from "../_request";
+import { MusicID } from "../../models/Music";
+
+export type GetMusicUrlReqData = {
+  id: MusicID;
+};
+
+/**
+ * 根据传入 id 获得一首歌资源的 url
+ * @returns url 或 null
+ */
+export async function getMusicUrlReq(
+  params: GetMusicUrlReqData
+): Promise<string | null> {
+  const res = await _request<{
+    data: {
+      url: string;
+    }[];
+  }>({
+    url: "/song/url",
+    method: "GET",
+    params,
+  });
+
+  if (!res || !res.data || !res.data.length) return null;
+
+  return res.data[0].url;
+}
