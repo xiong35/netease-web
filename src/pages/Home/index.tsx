b91b4572dc5cfad88c75d092389a79844f31cfb6
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-12-19 18:33:26 +0800

    feat(page): add home page recommend musics

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,11 @@ import "./index.scss";
 
 import Carousel from "./components/Carousel";
 
+// import { useRecommendedMusic } from "./hooks/useRecommendedMusic";
+
 function Home() {
+  // const { recommendMusic } = useRecommendedMusic();
+
   return (
     <div className="home">
       Home
