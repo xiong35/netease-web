502edf042644fe9a65e7980f81c41639d98447e5
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-12-20 16:26:24 +0800

    feat(component): add top bar place holder

--- src/components/TheTopBar/index.tsx
+++ src/components/TheTopBar/index.tsx
@@ -1,29 +1,32 @@
-import './index.scss'
+import "./index.scss";
 
-import Logo from './components/Logo/Logo'
-import PageChanger from './components/PageChanger/PageChanger'
-import SearchBar from './components/SearchBar/SearchBar'
-import Login from './components/Login/Login'
-import ThemeBtn from './components/ThemeBtn/ThemeBtn'
+import { observer } from "mobx-react-lite";
 
-import { observer } from "mobx-react-lite"
+import Login from "./components/Login/Login";
+import Logo from "./components/Logo/Logo";
+import PageChanger from "./components/PageChanger/PageChanger";
+import SearchBar from "./components/SearchBar/SearchBar";
+import ThemeBtn from "./components/ThemeBtn/ThemeBtn";
 
 function _TheTopBar() {
-	return (
-		<div className="c-the_top_bar">
-      <Logo />
-      <div className="c-the_top_bar-middle">
-        <PageChanger />
-        <SearchBar />
+  return (
+    <>
+      <div className="c-the_top_bar">
+        <Logo />
+        <div className="c-the_top_bar-middle">
+          <PageChanger />
+          <SearchBar />
+        </div>
+        <div className="c-the_top_bar-right">
+          <Login />
+          <ThemeBtn />
+        </div>
       </div>
-      <div className="c-the_top_bar-right">
-        <Login />
-        <ThemeBtn />
-      </div>
-		</div>
-	)
+      <div className="c-the_top_bar-place-holder"></div>
+    </>
+  );
 }
 
-const TheTopBar = observer(_TheTopBar)
+const TheTopBar = observer(_TheTopBar);
 
-export default TheTopBar
+export default TheTopBar;
