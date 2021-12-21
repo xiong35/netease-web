ff0edecd58edc71afb62d81dae461130e6c8a535
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-12-21 16:29:02 +0800

    feat(page): add musi detail page structure

--- src/pages/MusicDetail/index.tsx
+++ src/pages/MusicDetail/index.tsx
@@ -1,4 +1,3 @@
-
 import "./index.scss";
 
 // type MusicDetailProps = {
@@ -9,7 +8,43 @@ function MusicDetail(/* props: MusicDetailProps */) {
 
   return (
     <div className="music_detail">
-      MusicDetail
+      <div className="music_detail-title">
+        <div className="music_detail-title-music_name">music_name</div>
+        <div className="music_detail-title-artist_name">artist_name</div>
+      </div>
+
+      <div className="music_detail-song">
+        <div className="music_detail-song-disc">
+          <div className="music_detail-song-disc-prob">prob</div>
+          <div className="music_detail-song-disc-cover">
+            cover
+            <div className="music_detail-song-disc-cover-dots">dots</div>
+          </div>
+        </div>
+
+        <div className="music_detail-song-lyric">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore vero,
+          inventore consequuntur dolores consequatur ipsum quidem odio error est
+          pariatur magni sunt velit nostrum hic ullam perspiciatis, iusto nihil
+          dolorem.
+        </div>
+
+        <div className="music_detail-song-recommend">
+          <div className="music_detail-song-recommend-item">item</div>
+          <div className="music_detail-song-recommend-item">item</div>
+          <div className="music_detail-song-recommend-item">item</div>
+          <div className="music_detail-song-recommend-item">item</div>
+        </div>
+      </div>
+
+      <div className="music_detail-comments">
+        <div className="music_detail-comments-title">全部评论</div>
+        <div className="music_detail-comments-item">item</div>
+        <div className="music_detail-comments-item">item</div>
+        <div className="music_detail-comments-item">item</div>
+        <div className="music_detail-comments-item">item</div>
+        <div className="music_detail-comments-item">item</div>
+      </div>
     </div>
   );
 }
