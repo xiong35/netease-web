80f1bf3706737bce79d0a6fe968972c512861f37
gu can <68229998+gucan707@users.noreply.github.com>
2021-12-21 16:58:33 +0800

    feat(component): 完成asideBar收起展开

--- src/components/TheAsideBar/index.tsx
+++ src/components/TheAsideBar/index.tsx
@@ -5,39 +5,58 @@ import { useState } from "react";
 import Img from "../Img";
 import { myMusic } from "./data/myMusic";
 import { tabs } from "./data/tabs";
+import leftIcon from "./img/left.svg";
 
-// type TheAsideBarProps = {
-// };
-
-function TheAsideBar(/* props: TheAsideBarProps */) {
-  // const {} = props;
+function TheAsideBar() {
   const [tabSelected, setTabSelected] = useState(tabs[0]);
+  const [asideBarShrink, setAsideBarShrink] = useState(false);
 
   return (
-    <div className="aside_bar">
-      {tabs.map((tab) => (
+    <>
+      <div
+        className={"aside_bar-placeholder" + (asideBarShrink ? " shrink" : "")}
+      ></div>
+      <div className={"aside_bar" + (asideBarShrink ? " shrink" : "")}>
         <div
-          className={`aside_bar-tab ${tabSelected === tab ? "selected" : ""}`}
-          key={tab}
-          onClick={() => setTabSelected(tab)}
+          className={"aside_bar-arrow" + (asideBarShrink ? " show" : "")}
+          onClick={() => setAsideBarShrink(!asideBarShrink)}
         >
-          {tab}
-        </div>
-      ))}
-      <div className="aside_bar-title">我的音乐</div>
-      {myMusic.map((myMusicItem) => (
-        <div className="aside_bar-my_music">
           <Img
-            className="aside_bar-my_music-icon"
-            src={myMusicItem.icon}
-            alt=""
-            loadingMask={false}
+            src={leftIcon}
+            alt="arrow"
+            className={
+              "aside_bar-arrow-icon" + (asideBarShrink ? " right" : "")
+            }
           />
-          <span>{myMusicItem.content}</span>
         </div>
-      ))}
-      <div className="aside_bar-title">创建的歌单</div>
-    </div>
+        <div className="aside_bar-wrap">
+          {tabs.map((tab) => (
+            <div
+              className={`aside_bar-tab ${
+                tabSelected === tab ? "selected" : ""
+              }`}
+              key={tab}
+              onClick={() => setTabSelected(tab)}
+            >
+              {tab}
+            </div>
+          ))}
+          <div className="aside_bar-title">我的音乐</div>
+          {myMusic.map((myMusicItem) => (
+            <div className="aside_bar-my_music">
+              <Img
+                className="aside_bar-my_music-icon"
+                src={myMusicItem.icon}
+                alt=""
+                loadingMask={false}
+              />
+              <span>{myMusicItem.content}</span>
+            </div>
+          ))}
+          <div className="aside_bar-title">创建的歌单</div>
+        </div>
+      </div>
+    </>
   );
 }
 
