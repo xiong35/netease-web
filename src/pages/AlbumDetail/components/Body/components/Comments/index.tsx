a049b4b051d3827582133a6b2a71830beddd5828
DanmoSAMA <772523546@qq.com>
2021-12-22 12:40:50 +0800

    fix(page): album-detail 无评论时显示“暂无评论”

--- src/pages/AlbumDetail/components/Body/components/Comments/index.tsx
+++ src/pages/AlbumDetail/components/Body/components/Comments/index.tsx
@@ -9,35 +9,40 @@ import { SongListStore } from '../../../../../../mobx/songlist'
 function _Comments() {
   return (
     <div className="album_detail-body-comments">
-      <div className="album_detail-body-comments-hotest">
-        <div className="album_detail-body-comments-hotest-header">精彩评论</div>
-        <div className="album_detail-body-comments-hotest-list">
-          {SongListStore.hotComments.length
-            ? SongListStore.hotComments.map(item => (
-                <Comment data={item} key={item.commentId} />
-              ))
-            : ''}
+      {SongListStore.hotComments.length ? (
+        <div className="album_detail-body-comments-hotest">
+          <div className="album_detail-body-comments-hotest-header">
+            精彩评论
+          </div>
+          <div className="album_detail-body-comments-hotest-list">
+            {SongListStore.hotComments.map(item => (
+              <Comment data={item} key={item.commentId} />
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="album_detail-body-comments-newest">
-        <div
-          className="album_detail-body-comments-newest-header"
-          id="ad_co-newest_header"
-        >
-          最新评论({SongListStore.commentCount})
+      ) : ''}
+      {SongListStore.comments.length ? (
+        <div className="album_detail-body-comments-newest">
+          <div
+            className="album_detail-body-comments-newest-header"
+            id="ad_co-newest_header"
+          >
+            最新评论({SongListStore.commentCount})
+          </div>
+          <div className="album_detail-body-comments-newest-list">
+            {SongListStore.comments.map(item => (
+              <Comment data={item} key={item.commentId} />
+            ))}
+          </div>
         </div>
-        <div className="album_detail-body-comments-newest-list">
-          {SongListStore.comments.length
-            ? SongListStore.comments.map(item => (
-                <Comment data={item} key={item.commentId} />
-              ))
-            : ''}
-        </div>
-      </div>
-      {SongListStore.commentCount > SongListStore.commentLimit ? (
+      ) : ''}
+      {
+        !SongListStore.hotComments.length && !SongListStore.comments.length ? (
+          <div className="album_detail-body-comments-no_comments">暂无评论</div>
+        ) : ''
+      }
+      {SongListStore.commentCount > SongListStore.commentLimit && (
         <PageSelector />
-      ) : (
-        ''
       )}
     </div>
   )
