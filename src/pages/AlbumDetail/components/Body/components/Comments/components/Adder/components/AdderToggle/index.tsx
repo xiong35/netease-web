816c5be97195a5bc30d5cb1d8d127f9050d85ce8
DanmoSAMA <772523546@qq.com>
2021-12-23 15:38:24 +0800

    fix(page): album-detail 修复评论toggle的bug

--- src/pages/AlbumDetail/components/Body/components/Comments/components/Adder/components/AdderToggle/index.tsx
+++ src/pages/AlbumDetail/components/Body/components/Comments/components/Adder/components/AdderToggle/index.tsx
@@ -1,20 +1,20 @@
 import './index.scss'
 
 type AdderToggleProps = {
-  content: string
+  isEmpty: boolean
 }
 
 function AdderToggle(props: AdderToggleProps) {
-  const { content } = props
+  const { isEmpty } = props
   return (
     <div
       className={
-        content !== ''
+        !isEmpty
           ? 'album_detail-body-comments-add_comment-toggle'
           : 'album_detail-body-comments-add_comment-toggle empty'
       }
     >
-      {content !== '' ? '评论成功!' : '写点东西吧，内容不能为空哦!'}
+      {!isEmpty ? '评论成功!' : '写点东西吧，内容不能为空哦!'}
     </div>
   )
 }
--- src/pages/AlbumDetail/components/Body/components/Comments/components/Adder/index.tsx
+++ src/pages/AlbumDetail/components/Body/components/Comments/components/Adder/index.tsx
@@ -5,25 +5,35 @@ import { observer } from 'mobx-react-lite'
 import AdderToggle from './components/AdderToggle'
 import { useState } from 'react'
 
+// 设置timer为全局变量
+let timer: ReturnType<typeof setTimeout> | null = null
+
 function _Adder() {
   const [showToggle, setShowToggle] = useState(false)
   const [content, setContent] = useState('')
+  const [isEmpty, setIsEmpty] = useState(true)
+
 
   function submit() {
     const commentTextArea = document.querySelector(
       '.album_detail-body-comments-add_comment-text_area'
     ) as HTMLInputElement
-    const content = commentTextArea.value
-
     setShowToggle(true)
-    setTimeout(() => {
+
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => {
       setShowToggle(false)
     }, 2000)
 
     if (content !== '') {
       SongListStore.postComment(content)
-      setContent(content)
       commentTextArea.value = ''
+      setContent('')
+      setIsEmpty(false)
+    } else {
+      setIsEmpty(true)
     }
   }
 
@@ -31,9 +41,17 @@ function _Adder() {
     <div className="album_detail-body-comments-add_comment">
       <textarea
         className="album_detail-body-comments-add_comment-text_area"
+        onChange={() => {
+          const commentTextArea = document.querySelector(
+            '.album_detail-body-comments-add_comment-text_area'
+          ) as HTMLInputElement
+          setContent(commentTextArea.value)
+        }}
         onKeyDown={e => {
           if (e.key === 'Enter') {
             submit()
+            // 避免回车后产生多余换行符
+            e.preventDefault()
           }
         }}
       ></textarea>
@@ -45,7 +63,7 @@ function _Adder() {
       >
         评论
       </button>
-      {showToggle ? <AdderToggle content={content} /> : ''}
+      {showToggle ? <AdderToggle isEmpty={isEmpty} /> : ''}
     </div>
   )
 }
