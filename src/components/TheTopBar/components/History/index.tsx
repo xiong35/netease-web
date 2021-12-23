b2fe395288d592399c8ce75e52d59b8fe6e3320f
DanmoSAMA <772523546@qq.com>
2021-12-23 13:01:43 +0800

    fix(component): thetopbar 修复dropdown的字体和图标颜色等bug

--- src/components/TheTopBar/components/History/index.tsx
+++ src/components/TheTopBar/components/History/index.tsx
@@ -7,7 +7,7 @@ function History() {
     <div className="c-the_top_bar-middle-search_bar-dropdown-history">
       <div className="c-the_top_bar-middle-search_bar-dropdown-history-header">
         <h2>搜索历史</h2>
-        <img src={trashBin} className="icon" />
+        <img src={trashBin} className="c-the_top_bar-middle-search_bar-dropdown-history-header-icon" />
       </div>
       <div className="c-the_top_bar-middle-search_bar-dropdown-history-items"></div>
     </div>
