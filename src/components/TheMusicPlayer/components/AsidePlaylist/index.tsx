9e1921a5adf7af34fb27f595d76c0021c0522d0f
xiong35 <56919869+xiong35@users.noreply.github.com>
2021-12-25 16:46:31 +0800

    feat(component): add aside play list

---
+++ src/components/TheMusicPlayer/components/AsidePlaylist/index.tsx
@@ -0,0 +1,34 @@
+import "./index.scss";
+
+import { observer } from "mobx-react-lite";
+
+import { PlayStore } from "../../../../mobx/play";
+import { songsInfoFormat } from "../../../../utils/songsInfoFormat";
+import SongsList from "../../../SongsList";
+
+type AsidePlaylistProps = {
+  show: boolean;
+};
+
+function _AsidePlaylist(props: AsidePlaylistProps) {
+  const { show } = props;
+
+  const playlist = PlayStore.tracks;
+
+  return (
+    <div className={`aside_playlist ${show ? "" : "hide"}`}>
+      {playlist.length === 0 ? (
+        <div>没有歌曲呢</div>
+      ) : (
+        <SongsList
+          tableContents={songsInfoFormat(playlist)}
+          tableHeads={["音乐标题", "专辑", "时长"]}
+        ></SongsList>
+      )}
+    </div>
+  );
+}
+
+const AsidePlaylist = observer(_AsidePlaylist);
+
+export default AsidePlaylist;
